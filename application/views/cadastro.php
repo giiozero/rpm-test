e2c9cca21a510dfaf3197b0c2b191a35a29c3b53
Giovanni Vassallo <giovassallo@hotmail.com>
2018-10-02 00:05:22 -0300

    Add files via upload

--- application/views/cadastro.php
+++ application/views/cadastro.php
@@ -4,9 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="pt-br">
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Teste RPM - Cadastro</title>
 		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/mycustom.css">
+		<link rel="stylesheet" type="text/css" href="https://theforestbr.com/rpm/css/mycustom.css">
 	</head>
 	<body>
 		<div id="container">
@@ -141,6 +142,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.maskedinput/1.4.1/jquery.maskedinput.min.js"></script>
-		<script type='text/javascript' src="<?php echo base_url(); ?>js/mycustom.js"></script>
+		<script type='text/javascript' src="https://theforestbr.com/rpm/js/mycustom.js"></script>
 	</body>
 </html>
